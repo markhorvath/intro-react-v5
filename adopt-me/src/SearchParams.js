5c68938bacb440ae2203add88a4cc13dbaff3f5f
markhorvath <markhorvath7@gmail.com>
2020-01-04 18:23:07 -0500

    custom hook useDropdown implemented

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -4,9 +4,9 @@ import useDropdown from '/useDropdown';
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
-    const [animal, setAnimal] = useState("dog");
-    const [breed, setBreed] = useState("");
     const [breeds, setBreeds] = useState([]);
+    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
     return (
         <div className="search-params">
@@ -17,29 +17,8 @@ const SearchParams = () => {
                     onChange={e => setLocation(e.target.value)}
                     />
                 </label>
-                <label htmlFor="animal">
-                    Animal
-                    <select id="animal" value={animal}
-                    onChange={e => setAnimal(e.target.value)}
-                    onBlur={e => setAnimal(e.target.value)}>
-                    <option>All</option>
-                    {ANIMALS.map(animal => (
-                        <option key={animal} value={animal}>{animal}</option>
-                    ))}
-                    </select>
-                </label>
-                <label htmlFor="breed">
-                    Breed
-                    <select id="breed" value={breed}
-                    onChange={e => setBreed(e.target.value)}
-                    onBlur={e => setBreed(e.target.value)}
-                    disabled={!breeds.length}>
-                    <option>All</option>
-                    {breeds.map(breedStr => (
-                        <option key={breedStr} value={breedStr}>{breedStr}</option>
-                    ))}
-                    </select>
-                </label>
+                <AnimalDropdown />
+                <BreedDropdown />
                 <button>Submit</button>
             </form>
         </div>
