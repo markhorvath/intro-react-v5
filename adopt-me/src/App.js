a427d830c585ee9fcbf5d90e61b7ca72e0fe0e77
markhorvath <markhorvath7@gmail.com>
2020-01-04 18:18:38 -0500

    commit before implementing custom hook, still using individual animal and breed hooks

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+// import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(
-  //     "p",
-  //     { id: "test-paragraph" },
-  //     "This is a test parapgraph"
-  //   ),
-  //   React.createElement(Pet, {
-  //     name: "Zodd",
-  //     animal: "Dog",
-  //     breed: "Golden Retriever"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Charlie",
-  //     animal: "Rabbit",
-  //     breed: "whatever"
-  //   }),
-  //   React.createElement(Pet, { name: "Bear", animal: "Dog", breed: "Mutt" })
-  // ]);
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Zodd" animal="Dog" breed="Retriever" />
+{/*      <Pet name="Zodd" animal="Dog" breed="Retriever" />
       <Pet name="Zoe" animal="Dog" breed="Pug" />
-      <Pet name="Bo" animal="Cat" breed="Siamese" />
-
+      <Pet name="Bo" animal="Cat" breed="Siamese" />*/}
+      <SearchParams />
     </div>
   )
 };
