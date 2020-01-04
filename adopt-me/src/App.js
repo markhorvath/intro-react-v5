43a6d882555fb6ba8d82d55b27549b12d457051d
markhorvath <markhorvath7@gmail.com>
2020-01-03 20:46:35 -0500

    covert to jsx, reconfigure eslint

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -3,25 +3,34 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(
-      "p",
-      { id: "test-paragraph" },
-      "This is a test parapgraph"
-    ),
-    React.createElement(Pet, {
-      name: "Zodd",
-      animal: "Dog",
-      breed: "Golden Retriever"
-    }),
-    React.createElement(Pet, {
-      name: "Charlie",
-      animal: "Rabbit",
-      breed: "whatever"
-    }),
-    React.createElement(Pet, { name: "Bear", animal: "Dog", breed: "Mutt" })
-  ]);
+  // return React.createElement("div", { id: "something-important" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(
+  //     "p",
+  //     { id: "test-paragraph" },
+  //     "This is a test parapgraph"
+  //   ),
+  //   React.createElement(Pet, {
+  //     name: "Zodd",
+  //     animal: "Dog",
+  //     breed: "Golden Retriever"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Charlie",
+  //     animal: "Rabbit",
+  //     breed: "whatever"
+  //   }),
+  //   React.createElement(Pet, { name: "Bear", animal: "Dog", breed: "Mutt" })
+  // ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Zodd" animal="Dog" breed="Retriever" />
+      <Pet name="Zoe" animal="Dog" breed="Pug" />
+      <Pet name="Bo" animal="Cat" breed="Siamese" />
+
+    </div>
+  )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
