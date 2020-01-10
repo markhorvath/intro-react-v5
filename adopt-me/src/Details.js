394f5af59d57f4e45faba7c095f96f65efbc27eb
markhorvath <markhorvath7@gmail.com>
2020-01-09 21:17:42 -0500

    added Class component Details, rendered and configured babel for parcel

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import pet from '@frontendmasters/pet'
 // this will pretty print any object passed to it (such as props) to the DOM, good for debugging.  can also use devtools
 // const Details = props => {
 //     return (
@@ -6,10 +7,43 @@ import React from 'react';
 //         );
 // }
 
-const Details = props => {
-    return (
-        <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
-        );
+class Details extends React.Component {
+    state = { loading: true };
+
+    componentDidMount() {
+        pet.animal(this.props.id)
+            .then(({ animal }) => {
+                this.setState({
+                    name: animal.name,
+                    animal: animal.type,
+                    location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+                    description: animal.description,
+                    media: animal.photos,
+                    breed: animal.breeds.primary,
+                    loading: false
+                })
+            }, console.error);
+    }
+
+    render () {
+        if (this.state.loading) {
+            return <h1>loading...</h1>
+        }
+
+        const { animal, breed, location, description, name } =
+        this.state;
+
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Details;
