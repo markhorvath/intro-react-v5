6bca4ab0d5484343d3cf215d1b42c571534c2c1d
markhorvath <markhorvath7@gmail.com>
2020-01-11 16:46:41 -0500

    added Carousel component to Details, notes on unary plus and arrow funcs for event listeners

---
+++ adopt-me/src/Carousel.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+    state = {
+        photos: [],
+        active: 0
+    }
+
+    static getDerivedStateFromProps({ media }) {
+        let photos = ['https://placecorgi.com/600/600'];
+
+        if (media.length) {
+            photos = media.map(({ large }) => large);
+        };
+
+        return { photos };
+    };
+
+    //whenever you are passing functions down into children, or doing event listeners, use an arrow function because it guarantes that 'this' is correct
+    handleIndexClick = event => {
+        this.setState({
+            //the '+' is a unairy plus will turn a string to a number (because dataset is from the DOM and all from the DOM is a string)
+            active: +event.target.dataset.index
+        });
+    };
+
+    render() {
+        const { photos, active } = this.state;
+
+        return (
+            <div className="carousel">
+                <img src={photos[active]} alt="animal" />
+                <div className="carousel-smaller">
+                {/*in production don't put the eventlistener onClick on the img*/}
+                    {photos.map((photo, index) => (
+                        <img
+                            key={photo}
+                            onClick={this.handleIndexClick}
+                            data-index={index}
+                            src={photo}
+                            className={index === active ? "active" : ""}
+                            alt="animal-thumbnail"
+                        />
+                        ))}
+                </div>
+            </div>)
+    }
+};
+
+export default Carousel;
--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import pet from '@frontendmasters/pet'
+import pet from '@frontendmasters/pet';
+import Carousel from './Carousel';
 // this will pretty print any object passed to it (such as props) to the DOM, good for debugging.  can also use devtools
 // const Details = props => {
 //     return (
@@ -30,11 +31,12 @@ class Details extends React.Component {
             return <h1>loading...</h1>
         }
 
-        const { animal, breed, location, description, name } =
+        const { animal, breed, location, description, name, media } =
         this.state;
 
         return (
             <div className="details">
+                <Carousel media={media} />
                 <div>
                     <h1>{name}</h1>
                     <h2>{`${animal} - ${breed} - ${location}`}</h2>
